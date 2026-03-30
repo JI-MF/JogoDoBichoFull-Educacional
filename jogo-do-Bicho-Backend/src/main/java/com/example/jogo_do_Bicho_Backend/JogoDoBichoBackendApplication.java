af8a346b89117085820b554b7cfa01b6b64ccd39
Jose Ivan <joseivanmarinho10@gmail.com>
2026-03-30 17:51:32 -0300

    Adicionando frontend e backend

---
+++ jogo-do-Bicho-Backend/src/main/java/com/example/jogo_do_Bicho_Backend/JogoDoBichoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.jogo_do_Bicho_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogoDoBichoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogoDoBichoBackendApplication.class, args);
+	}
+
+}
---
+++ jogo-do-Bicho-Backend/src/test/java/com/example/jogo_do_Bicho_Backend/JogoDoBichoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jogo_do_Bicho_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogoDoBichoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
